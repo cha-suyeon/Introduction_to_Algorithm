a581642c9065a9e54ebbcbca75d76b6ed131bcba
cha-suyeon <suyeon.chaa@gmail.com>
2022-12-12 10:33:48 +0900

    1212

---
+++ coding/chapter3/Better_way19.py
@@ -0,0 +1,61 @@
+
+def get_stats(numbers):
+    minimum = min(numbers)
+    maximum = max(numbers)
+    return minimum, maximum
+
+lengths = [63, 73, 72, 60, 67, 66, 71, 61, 72, 70]
+
+minimum, maximum = get_stats(lengths) # 반환 값이 두 개
+
+print(f'최소: {minimum}, 최대: {maximum}')
+
+
+first, second = 1, 2
+assert first == 1
+assert second == 2
+
+def my_function():
+    return 1, 2
+
+first, second = my_function()
+assert first == 1
+assert second == 2
+
+
+def get_avg_ratio(numbers):
+    average = sum(numbers) / len(numbers)
+    scaled = [x / average for x in numbers]
+    scaled.sort(reverse=True)
+    return scaled
+
+longest, *middle, shortest = get_avg_ratio(lengths)
+
+print(f'최대 길이: {longest:>4.0%}')        # 최대 길이: 108%
+print(f'최소 길이: {shortest:>4.0%}')       # 최소 길이:  89%
+
+
+
+
+def get_stats(numbers):
+    minimum = min(numbers)
+    maximum = max(numbers)
+    count = len(numbers)
+    average = sum(numbers) / count
+    
+    sorted_numbers = sorted(numbers)
+    middle = count // 2
+    if count % 2 == 0:
+        lower = sorted_numbers[middle - 1]
+        upper = sorted_numbers[middle]
+        median = (lower + upper) /2
+    else:
+        median = sorted_numbers[middle]
+        
+    return minimum, maximum, average, median, count
+
+minimum, maximum, average, median, count = get_stats(lengths)
+
+print(f'최소 길이: {minimum}, 최대 길이: {maximum}')            # 최소 길이: 60, 최대 길이: 73
+print(f'평균: {average}, 중앙값: {median}, 개수: {count}')      # 평균: 67.5, 중앙값: 68.5, 개수: 10
+
