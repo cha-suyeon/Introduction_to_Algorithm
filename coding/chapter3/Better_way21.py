c959f9cc4d331910773950fbe292f9df3d5ce3f6
cha-suyeon <suyeon.chaa@gmail.com>
2022-12-15 10:19:48 +0900

    1215

---
+++ coding/chapter3/Better_way21.py
@@ -0,0 +1,13 @@
+# 정렬한 리스트 앞쪽에 우선순위를 부여한 몇몇 숫자를 위치시키기
+def sort_priority(values, group):
+    def helper(x):
+        if x in group:
+            return (0, x)
+        return (1, x)
+    values.sort(key=helper)
+
+numbers = [8, 3, 1, 2, 5, 4, 7, 6]
+group = {2, 3, 5, 7}
+sort_priority(numbers, group)
+print(numbers)
+
