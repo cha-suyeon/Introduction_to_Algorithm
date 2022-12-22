db59b91ecab55e3ef843a2cdde7018f9a295d58a
cha-suyeon <suyeon.chaa@gmail.com>
2022-12-22 14:22:06 +0900

    1222

---
+++ programmers/lv0/배열2.py
@@ -0,0 +1,16 @@
+# 나이 출력
+def solution(age):
+    answer = 2023 - age
+    return answer
+
+# 각도기
+def solution(angle):
+    if angle >= 180:
+        answer = 4
+    elif angle > 90:
+        answer = 3
+    elif angle >= 90:
+        answer = 2
+    else:
+        answer = 1
+    return answer
